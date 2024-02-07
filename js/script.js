c1d64499d9294df9f22ccb6abc36182d011e0b09
slupss <157841289+slupss@users.noreply.github.com>
2024-02-07 12:33:30 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,142 @@
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+      speed: 1200,
+      adaptiveHeight: false,
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+      responsive: [
+          {
+              breakpoint: 992,
+              settings: {
+                  dots: true,
+                  arrows: false
+              }
+          }
+      ]
+  });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    // $('.catalog-item__link').each(function(i) {
+    //   $(this).on('click',function(e) {
+    //     e.preventDefault();
+    //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    //   })
+    // });
+
+    // $('.catalog-item__back').each(function(i) {
+    //   $(this).on('click',function(e) {
+    //     e.preventDefault();
+    //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    //   })
+    // });
+
+    function toggleSlide(item) {
+      $(item).each(function(i) {
+        $(this).on('click',function(e) {
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+      });
+    };
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    //Modal
+
+    $('[data-modal="consultation"]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click',function() {
+      $('.overlay, #consultation, #thans, #order').fadeOut('slow');
+    });
+
+
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      })
+    });
+
+    // $('#consultation-form').validate();
+    // $('#consultation form').validate({
+    //   rules: {
+    //     name: {
+    //       required: true,
+    //       minlength: 2
+    //     },
+    //     phone:'required',
+    //     email: {
+    //       required: true,
+    //       email: true
+    //     }
+    //   },
+    //     messages: {
+    //     name: {
+    //       required: "Пожалуйста введите свое имя",
+    //       minlength: jQuery.validator.format("Введите {0} cимвола!")
+    //     },
+    //     phone:"Пожалуйста введите свой номер телефона",
+    //     email: {
+    //       required: "Пожалуйста введите свою почту",
+    //       email: "Ваш формат должен быть name@domain.com"
+    //     }
+    //   }
+    // });
+    // $('#order form').validate();
+
+    function valideForms(form){
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2
+          },
+          phone:'required',
+          email: {
+            required: true,
+            email: true
+          }
+        },
+          messages: {
+          name: {
+            required: "Пожалуйста введите свое имя",
+            minlength: jQuery.validator.format("Введите {0} cимвола!")
+          },
+          phone:"Пожалуйста введите свой номер телефона",
+          email: {
+            required: "Пожалуйста введите свою почту",
+            email: "Ваш формат должен быть name@domain.com"
+          }
+        }
+      });
+    };
+    
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+
+    new WOW().init();
+    
+});
+
+
